1387eb689c5ef7b29270d310dbee819fde139446
Ritu Rajput <riturajput09@gmail.com>
2022-06-16 10:01:40 +0530

    added new log

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -264,6 +264,6 @@ const bill = 430
 const tip = bill >=50 && bill <=300 ? bill * 0.15 : bill * 0.2;
 console.log(`the bill was ${bill}, the tip was ${tip} and the total value ${bill + tip}`);
 console.log("hello");
-
+console.log("bye");
 
 
