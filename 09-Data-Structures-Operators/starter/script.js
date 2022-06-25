b27b5564042adce396283283fcb32b4afd30fcc8
Other <other@1MG-LPTP-647.local>
2022-06-25 11:31:46 +0530

    add starter/script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,18 +12,46 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-};
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.
+    mainMenu[mainIndex]]
+  }
+};  
+const
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// switching variables
+//const temp = main;
+//main = secondary;
+// secondary= temp;
+//console.log(main, secondary);
+
+
+
+[secondary, main] = [main, secondary];
+console.log(main, secondary);
+
+// receive two return value from a function
+//const[starter, maincourse] = restaurant.order(2, 0);
+//console.log(starter, maincourse);
+
+//const nested = [2, 4, [5, 6]];
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+//nested destructuring
+const nested = [2, 4, [5, 6]];
+//const[i,  ,j] = nested;
+const[i,  ,[j, k] = nested;
+console.log(i, j, k);
+
